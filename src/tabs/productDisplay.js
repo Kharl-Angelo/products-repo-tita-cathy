4069f741a5cd4d066006ca6620e63f4163dae21e
kharl <kharl@hehe.com>
2025-02-01 08:06:35 +0800

    last push for 1-31-25

--- src/tabs/productDisplay.js
+++ src/tabs/productDisplay.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useRef } from "react";
 import { MyContext } from "../App";
 import {
   TableContainer,
@@ -9,10 +9,17 @@ import {
   TableBody,
   Stack,
   Button,
+  TextField,
 } from "@mui/material";
 
 const ProductDisplay = () => {
   const { products, setProducts } = useContext(MyContext);
+  const [isUpdating, setIsUpdating] = useState(null);
+  const [newUpdatedValue, setNewUpdatedValue] = useState({});
+
+  const refNewProductID = useRef("");
+  const refNewProductName = useRef("");
+  const refNewProductPrice = useRef("");
 
   const tableTitles = [
     "Product ID",
@@ -23,6 +30,18 @@ const ProductDisplay = () => {
 
   const handleDeleteProduct = (index) => {
     setProducts((prevProducts) => prevProducts.filter((_, i) => index !== i));
+  };
+
+  const handleUpdateProduct = (index) => {
+    setIsUpdating(index === isUpdating ? null : index);
+  };
+
+  const handleConfirmUpdate = (index) => {
+    setProducts((prevProducts) =>
+      prevProducts.map((prevProduct, i) =>
+        index === i ? console.log("true") : console.log("not true")
+      )
+    );
   };
 
   return (
@@ -38,18 +57,65 @@ const ProductDisplay = () => {
         <TableBody>
           {products.map((product, index) => (
             <TableRow key={product.ProductID}>
-              <TableCell>{product.ProductID}</TableCell>
-              <TableCell>{product.ProductName}</TableCell>
-              <TableCell>{product.ProductPrice}</TableCell>
+              <TableCell>
+                {isUpdating === index ? (
+                  <TextField
+                    defaultValue={product.ProductID}
+                    inputRef={refNewProductID}
+                    variant="standard"
+                  />
+                ) : (
+                  product.ProductID
+                )}
+              </TableCell>
+              <TableCell>
+                {isUpdating === index ? (
+                  <TextField
+                    defaultValue={product.ProductName}
+                    inputRef={refNewProductName}
+                    variant="standard"
+                  />
+                ) : (
+                  product.ProductName
+                )}
+              </TableCell>
+              <TableCell>
+                {isUpdating === index ? (
+                  <TextField
+                    defaultValue={product.ProductPrice}
+                    inputRef={refNewProductPrice}
+                    variant="standard"
+                  />
+                ) : (
+                  product.ProductPrice
+                )}
+              </TableCell>
+
               <TableCell>
                 <Stack direction="row">
-                  <Button variant="outlined">Update</Button>
-                  <Button
-                    variant="outlined"
-                    onClick={() => handleDeleteProduct(index)}
-                  >
-                    Delete
-                  </Button>
+                  {isUpdating === index ? (
+                    <Button onClick={() => handleConfirmUpdate(index)}>
+                      Confirm
+                    </Button>
+                  ) : (
+                    <Button
+                      onClick={() => handleUpdateProduct(index)}
+                      variant="outlined"
+                    >
+                      Update
+                    </Button>
+                  )}
+
+                  {isUpdating === index ? (
+                    <Button onClick={() => setIsUpdating(null)}>Cancel</Button>
+                  ) : (
+                    <Button
+                      variant="outlined"
+                      onClick={() => handleDeleteProduct(index)}
+                    >
+                      Delete
+                    </Button>
+                  )}
                 </Stack>
               </TableCell>
             </TableRow>
